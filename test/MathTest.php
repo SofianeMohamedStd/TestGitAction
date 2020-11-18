3be573a5f05780fde1cd760fe6662ad3dff2c3c3
SofianeMohamedStd <namoune.sofianemohmaed@gmail.com>
2020-11-18 22:45:22 +0100

    first commit

---
+++ test/MathTest.php
@@ -0,0 +1,22 @@
+<?php
+
+use App\Math;
+
+use PHPUnit\Framework\TestCase;
+
+class MathTest extends TestCase{
+    public function testFibonacci() {
+        $math = new Math();
+        $this->assertEquals(34, $math->fibonacci(9));
+    }
+
+    /*public function testFactorial() {
+        $math = new Math();
+        $this->assertEquals(120, $math->factorial(5));
+    }*/
+
+    /*public function testFactorialGreaterThanFibonacci() {
+        $math = new Math();
+        $this->assertTrue($math->factorial(6) > $math->fibonacci(6));
+    }*/
+}
